2dab0e7426b95b5be41d634e527638022fbbffbf
Miles Rausch <miles@milesrausch.com>
2017-07-21 15:35:52 -0400

    Add GitHub pages reference. Maybe it'll work.

--- scripts/app.js
+++ scripts/app.js
@@ -2,7 +2,7 @@
     'use strict';
 
     if ('serviceWorker' in navigator) {
-        navigator.serviceWorker.register('/sw.js').then(function() {
+        navigator.serviceWorker.register('../sw.js').then(function() {
             console.log('Installed the Service Worker!');
         }).catch(function(err) {
             console.error('Unable to register service worker:', err);

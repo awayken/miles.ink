f02b40c51f16ddeb78e8c2b020f4ca8098c3a8e8
Miles Rausch <miles@milesrausch.com>
2020-07-10 08:44:10 -0500

    Prettier versions of these files

--- .eleventy.js
+++ .eleventy.js
@@ -32,70 +32,73 @@ const monthsShort = [
 
 module.exports = function (eleventyConfig) {
     // {{ variable | jsonify }}
-    eleventyConfig.addFilter('jsonify', function (variable) {
+    eleventyConfig.addFilter("jsonify", function (variable) {
         return JSON.stringify(variable);
     });
 
     // {{ variable | log }}
-    eleventyConfig.addFilter('log', function (variable) {
+    eleventyConfig.addFilter("log", function (variable) {
         console.log(variable);
     });
 
     // {{ arrayVariable | sentence }}
     eleventyConfig.addFilter("sentence", function (arrayVariable) {
-        return arrayVariable.join(' ');
+        return arrayVariable.join(" ");
     });
 
-    const makeIntoCSTDate = (dateVariable) => dateVariable.getDate ? dateVariable : new Date(`${dateVariable} CST`);
+    const makeIntoCSTDate = (dateVariable) =>
+        dateVariable.getDate ? dateVariable : new Date(`${dateVariable} CST`);
 
     // {{ dateVariable | shortdate }}
     eleventyConfig.addFilter("shortdate", function (dateVariable) {
         dateVariable = makeIntoCSTDate(dateVariable);
 
-        return `${dateVariable.getDate()} ${
-            monthsShort[dateVariable.getMonth()]
-        } ${dateVariable.getFullYear()}`;
+        return `${dateVariable.getDate()} ${monthsShort[dateVariable.getMonth()]} ${dateVariable.getFullYear()}`;
     });
 
     // {{ dateVariable | longdate }}
     eleventyConfig.addFilter("longdate", function (dateVariable) {
         dateVariable = makeIntoCSTDate(dateVariable);
 
-        return `${dateVariable.getDate()} ${
-            monthsLong[dateVariable.getMonth()]
-        } ${dateVariable.getFullYear()}`;
+        return `${dateVariable.getDate()} ${monthsLong[dateVariable.getMonth()]} ${dateVariable.getFullYear()}`;
     });
 
     // {{ array | where: key,value }}
-    eleventyConfig.addFilter('where', function (array, key, value) {
-        return array.filter(item => {
-            const keys = key.split('.');
+    eleventyConfig.addFilter("where", function (array, key, value) {
+        return array.filter((item) => {
+            const keys = key.split(".");
             const reducedKey = keys.reduce((object, key) => {
-            return object[key];
+                return object[key];
             }, item);
 
-            return (reducedKey === value ? item : false);
+            return reducedKey === value ? item : false;
         });
     });
 
     // {% buildTime %}
-    eleventyConfig.addShortcode("buildTime", function() {
+    eleventyConfig.addShortcode("buildTime", function () {
         const dateTime = new Date();
         return dateTime;
     });
 
-    eleventyConfig.addCollection("news", collection => {
-        return collection.getFilteredByGlob("posts/*.md").sort((a, b) => b.date - a.date);
+    eleventyConfig.addCollection("news", (collection) => {
+        return collection
+            .getFilteredByGlob("posts/*.md")
+            .sort((a, b) => b.date - a.date);
     });
 
-    eleventyConfig.addCollection("nav", collection => {
-        return collection.getFilteredByTag("page").sort((item1, item2) => item1.data.nav_order - item2.data.nav_order);
+    eleventyConfig.addCollection("nav", (collection) => {
+        return collection
+            .getFilteredByTag("page")
+            .sort(
+                (item1, item2) => item1.data.nav_order - item2.data.nav_order
+            );
     });
 
     // Copy the asset directories to the compiled site folder
-    eleventyConfig.addPassthroughCopy('files');
-    eleventyConfig.addPassthroughCopy('scripts');
-    eleventyConfig.addPassthroughCopy('styles');
+    eleventyConfig.addPassthroughCopy("files");
+    eleventyConfig.addPassthroughCopy("scripts");
+    eleventyConfig.addPassthroughCopy("styles");
     eleventyConfig.addPassthroughCopy({ favicons: "/" });
 
     eleventyConfig.setFrontMatterParsingOptions({
@@ -106,7 +109,7 @@ module.exports = function (eleventyConfig) {
     return {
         dir: {
             input: "./",
-            output: "./_site"
-        }
+            output: "./_site",
+        },
     };
 };
